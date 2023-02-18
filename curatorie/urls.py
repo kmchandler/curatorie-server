3d3f9c8bfe0e17cbecebbe0167fc5754aaccb8bc
Kristen Chandler <krismchandler@gmail.com>
2023-02-18 10:16:46 -0600

    setup models, views, fixtures for share requests

--- curatorie/urls.py
+++ curatorie/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
 from rest_framework import routers
-from curatorieapi.views import register_user, check_user, BoardView, GiftCardView, InspoCardView, ListCardView, PurchaseCardView, SharedBoardView, UserView, BoardTypeView, IconView
+from curatorieapi.views import register_user, check_user, BoardView, GiftCardView, InspoCardView, ListCardView, PurchaseCardView, SharedBoardView, UserView, BoardTypeView, IconView, ShareRequestView
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'users', UserView, 'user')
@@ -14,6 +14,7 @@ router.register(r'inspo_cards', InspoCardView, 'inspo_card')
 router.register(r'list_cards', ListCardView, 'list_card')
 router.register(r'purchase_cards', PurchaseCardView, 'purchase_card')
 router.register(r'shared_boards', SharedBoardView, 'shared_board')
+router.register(r'share_requests', ShareRequestView, 'share_request')
 router.register(r'board_types', BoardTypeView, 'board_type')
 router.register(r'icons', IconView, 'icon')
 
---
+++ curatorieapi/models/share_request.py
@@ -0,0 +1,8 @@
+from django.db import models
+from .user import User
+from .board import Board
+
+
+class ShareRequest(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    board = models.ForeignKey(Board, on_delete=models.CASCADE)

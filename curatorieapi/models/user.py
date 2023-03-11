3b01cd71424fbd074004c721bbc9415f28917b28
Kristen Chandler <krismchandler@gmail.com>
2023-03-11 13:14:30 -0600

    added unique constraints and reseeded

--- curatorieapi/models/user.py
+++ curatorieapi/models/user.py
@@ -5,7 +5,7 @@ class User(models.Model):
     uid = models.CharField(max_length=50)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    username = models.CharField(max_length=50)
+    username = models.CharField(max_length=50, unique=True)
     image_url = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
 
